18f4764a11a22fac5e209832bb7301318838bb59
jigaji <ts.saglara@gmail.com>
2022-11-16 14:06:10 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Main from './components/Main';
+import AddPost from './components/AddPost';
+import PostView from './components/PostView';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="page">
+        <Routes>
+          <Route path="/posts/new" component={AddPost} />
+          <Route path="/posts/:id" component={PostView} />
+          <Route path="/" component={Main} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Post.js
@@ -0,0 +1,27 @@
+import { useNavigate } from "react-router-dom";
+import PropTypes from 'prop-types';
+
+export default function Post({ post }) {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate.push(`/posts/${post.id}`);
+  }
+
+  const dateFormat = new Date(post.created);
+
+  return (
+    <div className="Post" onClick={handleClick}>
+      <div className="Post__date">Дата создания: {dateFormat.toLocaleDateString()}</div>
+      <div className="Post__content">{post.content}</div>
+    </div>
+  );
+}
+
+Post.propTypes = {
+  post: PropTypes.shape({
+    id: PropTypes.number,
+    created: PropTypes.number,
+    content: PropTypes.string,
+  }).isRequired,
+}
